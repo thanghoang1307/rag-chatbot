9db48a9b7cc9a8aabd0b85d34d465141e7ce0392
admin <thang.hoang1307@gmail.com>
2024-11-22 17:04:51 +0700

    Facebook URL callback

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -31,40 +31,9 @@ const handlerGetMethod = (req, res) => {
 
 const handlerPostMethod = async (req, res) => {
   try {
-    const config = JSON.parse(process.env.NEXT_PUBLIC_CONFIG || {});
-    const field = req.body.entry[0].changes?.[0]?.field;
-    const page_id = req.body.entry[0].id;
-    const page = config.data.find((page) => {
-      return page.id == page_id;
-    })
-
-    if (field) {
-      const item = req.body.entry[0].changes[0].value.item;
-      const verb = req.body.entry[0].changes[0].value.verb;
-      if (item == 'comment' && verb == 'add') {
-        const comment_id = req.body.entry[0].changes[0].value.comment_id;
-        const data = await hideComment(comment_id, page.access_token);
-        return { success: true, data };
-      } else {
-        return { success: true, message: 'not comment' };
-      }
-    } else {
-      return { success: true, message: 'not feed' };
-    }
+    console.log(req);
   } catch (error) {
     return { success: false, message: error.message };
   }
 }
-
-async function hideComment(comment_id, access_token) {
-  try {
-    const url = `https://graph.facebook.com/v21.0/${comment_id}?access_token=${access_token}`;
-    const response = await axios.post(url, null, { is_hidden: true, timeout: 10000 });
-    return response;
-  } catch (error) {
-    console.error("Lỗi khi gọi API:", error);
-    throw error;
-  }
-}
-
 
