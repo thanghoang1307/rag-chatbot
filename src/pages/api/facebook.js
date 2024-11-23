b3c6dba40be02ad4edf747d38d8dbe256f68dbd8
admin <thang.hoang1307@gmail.com>
2024-11-23 12:15:27 +0700

    U

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -4,6 +4,7 @@ import { openai } from '@ai-sdk/openai';
 import { generateText, streamText, tool, convertToCoreMessages } from 'ai';
 import { z } from 'zod';
 import { findRelevantContent } from '@/lib/ai/embedding';
+import { constants } from 'buffer';
 
 export default async function handler(req, res) {
   try {
@@ -39,7 +40,7 @@ const handlerPostMethod = async (req, res) => {
     const pageId = await req.body.entry[0].id;
     const customerId = await req.body.entry[0].messaging[0].sender.id;
     const conversations = await getConversation(pageId, customerId);
-    console.log(conversations);
+    console.log({conversation: conversations});
     return {success: true, message: 'ok'};
     const messages = conversations.messages.data;
     
@@ -111,6 +112,7 @@ async function getConversation(pageId, customerId) {
     const accessToken = getPageAccessToken(pageId);
     const url = `https://graph.facebook.com/v21.0/${customerId}/conversations?fields=participants,messages{message,from}&access_token=${accessToken}`;
     const response = await axios.get(url, null, {timeout: 10000 });
+    console.log(response);
     return response;
   } catch (error) {
     console.error("Lỗi khi get Conversation:", error.message);
