1e4e997c91ee5337153712c7c0600b38beabd65d
admin <thang.hoang1307@gmail.com>
2024-11-23 09:53:51 +0700

    Facebook demo

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -1,4 +1,9 @@
 import axios from 'axios';
+import { createResource } from '@/lib/actions/resources';
+import { openai } from '@ai-sdk/openai';
+import { generateText, streamText, tool } from 'ai';
+import { z } from 'zod';
+import { findRelevantContent } from '@/lib/ai/embedding';
 
 export default async function handler(req, res) {
   try {
@@ -31,7 +36,35 @@ const handlerGetMethod = (req, res) => {
 
 const handlerPostMethod = async (req, res) => {
   try {
-    return { success: true, message: 'ok' };
+    const message = await req.body.entry[0].message.text;
+    console.log(message);
+    const result = generateText({
+      model: openai('gpt-4o'),
+      messages,
+      system: `You are a telesales of a Masterise Homes company. Check Masterise Homes's knowledge base before answering any questions.
+      Only respond to questions using information from tool calls.
+      if no relevant information is found in the tool calls, respond user that you haven't known and tell them to call 0933 894 980 to know more in Vietnamese`,
+      tools: {
+        addResource: tool({
+          description: `remove "Vừng ơi mở ra" and add a resource to your knowledge base.
+            If the user provides sentence that contains "Vừng ơi mở ra", use this tool without asking for confirmation.`,
+          parameters: z.object({
+            content: z
+              .string()
+              .describe('the content or resource to add to the knowledge base'),
+          }),
+          execute: async ({ content }) => createResource({ content }),
+        }),
+        getInformation: tool({
+          description: `get information from your knowledge base to answer questions.`,
+          parameters: z.object({
+            question: z.string().describe('the users question'),
+          }),
+          execute: async ({ question }) => findRelevantContent(question),
+        }),
+      },
+    });
+    return {success: true, message: result.text};
   } catch (error) {
     return { success: false, message: error.message };
   }
