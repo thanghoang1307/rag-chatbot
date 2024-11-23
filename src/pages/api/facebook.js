3ff536165438c11f0b5e9ed22347fa6e49dc4720
admin <thang.hoang1307@gmail.com>
2024-11-23 12:03:08 +0700

    U

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -39,8 +39,8 @@ const handlerPostMethod = async (req, res) => {
     const pageId = await req.body.entry[0].id;
     const customerId = await req.body.entry[0].messaging[0].sender.id;
     const conversations = await getConversation(pageId, customerId);
-    const messages = conversations.data[0].messages.data;
-    console.log(messages)
+    const messages = conversations.messages.data;
+    
     messages.map(msg => {
       let role;
       
@@ -54,6 +54,8 @@ const handlerPostMethod = async (req, res) => {
 
       return {role, content};
     });
+
+    console.log(messages)
 
     const result = generateText({
       model: openai('gpt-4o'),
