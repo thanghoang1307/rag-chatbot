1dd5796836c054a5ea7ce9705206b8a595c28b0f
admin <thang.hoang1307@gmail.com>
2024-11-25 10:37:40 +0700

    Push

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -58,6 +58,8 @@ const handlerPostMethod = async (req, res) => {
 
     messages = messages.slice(0, 5).reverse();
 
+    console.log(messages);
+
     const result = await generateText({
       model: openai('gpt-4o'),
       messages,
