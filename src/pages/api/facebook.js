6bf789d8186da2372e0d9a0eb4788872355ce484
admin <thang.hoang1307@gmail.com>
2024-11-25 17:44:19 +0700

    Push

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -11,7 +11,7 @@ export default async function handler(req, res) {
     if (req.method == 'GET') {
       handlerGetMethod(req, res);
     } else {
-      const result = handlerPostMethod(req, res);
+      const result = await handlerPostMethod(req, res);
       res.status(200).json({message: 'done'});
     }
   } catch (error) {
