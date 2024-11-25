22dfb445c76562f21a13c908b1e9ffa644a14cf9
admin <thang.hoang1307@gmail.com>
2024-11-25 17:16:33 +0700

    Push

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -98,6 +98,9 @@ const handlerPostMethod = async (req, res) => {
 
 async function sendMessage(pageId, recipientId, message) {
   try {
+    if(customerId == '7899343366769040') {
+      const typing_off = await axios.post(`https://graph.facebook.com/v21.0/${pageId}/messages?recipient={id:${customerId}}&sender_action=typing_off&access_token=${accessToken}`);
+    }
     const accessToken = getPageAccessToken(pageId);
     const url = `https://graph.facebook.com/v21.0/${pageId}/messages?recipient={id:${recipientId}}&message={text:'${message}'}&messaging_type=RESPONSE&access_token=${accessToken}`;
     console.log(url);
@@ -112,15 +115,12 @@ async function sendMessage(pageId, recipientId, message) {
 async function getConversation(pageId, customerId) {
   try {
     if(customerId == '7899343366769040') {
-      const typing_on = axios.post(`https://graph.facebook.com/v21.0/${pageId}/messages?recipient={id:${customerId}}&sender_action=typing_on&access_token=${accessToken}`);
+      const typing_on = await axios.post(`https://graph.facebook.com/v21.0/${pageId}/messages?recipient={id:${customerId}}&sender_action=typing_on&access_token=${accessToken}`);
     }
     const accessToken = getPageAccessToken(pageId);
     const url = `https://graph.facebook.com/v21.0/${pageId}/conversations?platform=MESSENGER&user_id=${customerId}&fields=participants,messages{message,from}&access_token=${accessToken}`;
     console.log(url);
     const response = await axios.get(url, null, {timeout: 10000 });
-    if(customerId == '7899343366769040') {
-      const typing_off = axios.post(`https://graph.facebook.com/v21.0/${pageId}/messages?recipient={id:${customerId}}&sender_action=typing_off&access_token=${accessToken}`);
-    }
     return response.data;
   } catch (error) {
     console.error("Lỗi khi get Conversation:", error.message);
