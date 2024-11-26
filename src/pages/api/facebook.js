8ff3d7eb5ae372ab342729ff6a49a7273af7d2a5
admin <thang.hoang1307@gmail.com>
2024-11-26 09:35:47 +0700

    Push

--- src/pages/api/facebook.js
+++ src/pages/api/facebook.js
@@ -56,13 +56,12 @@ const handlerPostMethod = async (reqBody) => {
     });
 
     messages = messages.slice(0, 5).reverse();
-    console.log(messages[0]);
-    console.log(messages[4]);
+    console.log(messages);
     console.log("Start generate text");
     const { text } = await generateText({
       model: openai('gpt-4o-mini'),
       messages,
-      maxSteps: 5,
+      maxSteps: 3,
       system: `Bạn là nhân viên chăm sóc khách hàng của Masterise Homes và bạn sẽ trả lời các câu hỏi của khách hàng về Công ty cũng như các dự án thuộc Công ty. 
       Hãy dùng đại từ xưng hô gọi khách hàng là Anh/Chị, còn bạn dùng đại từ xưng hô là Em. 
       Hãy trả lời câu hỏi của khách hàng một cách lễ phép, đầy đủ và tôn trọng. Sử dụng các từ như 'Dạ thưa', 'Xin chào quý khách', 'Em xin phép giải thích', 'Rất cảm ơn quý khách đã hỏi',... để thể hiện thái độ lịch sự và chuyên nghiệp.. 
